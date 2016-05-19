3499e73854c1a6c60ef76e5757f83d032ca4d3cf
Mateusz Jałowiec <Mateusz Jaowiec>
2016-05-19 09:54:42 +0200

    ZernikeMoments library fix

--- src/main/java/tletters/featureextraction/ZernikeMoments.java
+++ src/main/java/tletters/featureextraction/ZernikeMoments.java
@@ -1,9 +1,6 @@
 package tletters.featureextraction;
 
-import org.apache.commons.math3.complex.*;
-
-//po dodaniu - compile group: 'org.apache.commons', name: 'commons-math3', version: '3.2'
-//i tak świeci wszystko na czerwono, postaram sie to ogarnac jutro
+import org.apache.commons.math3.complex.Complex;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
@@ -12,23 +9,23 @@ import java.util.Arrays;
 public class ZernikeMoments implements ExtractionAlgorithm {
 
     private int imsize;
-    private static int rzad = 10;   // wyzszy rzad -> wieksza dokladnosc, zbyt duzy rzad -> bledy zaokroglen; 10<rzad<45
+    private static int order = 15;
 
     ZernikeMoments(){}
-    ZernikeMoments(int rzad){
-        this.rzad = rzad;
+    ZernikeMoments(int order){
+        this.order = order;
     }
 
 
     public double[] extractFeatures(BufferedImage bufferedImage) {
 
         if(bufferedImage == null){
-            System.out.println("Brak obrazka!!!");
+            System.out.println("There is no image!");
             return null;
         }
 
         if(bufferedImage.getHeight() != bufferedImage.getWidth()){
-            System.out.println("Obrazek nie jest prostokatny!!! (zostanie obciety do krotszej krawedzi)");
+            System.out.println("the image is not square, it will be cropped!");
             if(bufferedImage.getHeight() < bufferedImage.getWidth()){
                 imsize=bufferedImage.getHeight();
             }else{
@@ -42,7 +39,7 @@ public class ZernikeMoments implements ExtractionAlgorithm {
             for( int j = 0; j < imsize; j++ )
                 pixels[i][j] = bufferedImage.getRGB(i, j);
 
-        MyZernike zs1=zernike_bf(imsize,rzad);
+        MyZernike zs1=zernike_bf(imsize,order);
         Complex[] v1=zernike_mom(pixels,zs1);
         double[] v1ABS = new double[v1.length];
         for(int i=0; i<v1.length; i++)
@@ -51,7 +48,7 @@ public class ZernikeMoments implements ExtractionAlgorithm {
     }
 
 
-    private MyZernike zernike_bf(int SZ, int ORDER){ // Ok - sprawdzone
+    private MyZernike zernike_bf(int SZ, int ORDER){
         int[] F=factorial(ORDER);
 
         ArrayList<int[]> pq=zernike_orderlist(ORDER);
@@ -120,24 +117,24 @@ public class ZernikeMoments implements ExtractionAlgorithm {
                         R = R + Rmns[ORDER + m][ORDER + n][s] * (Math.pow(rho, (m - 2 * s)));
 
                     }
-                    ZBF[y-1][x-1][flat] = multyplay(new Complex(R,0) , exp(new Complex(0, n * theta)) );
+                    ZBF[y-1][x-1][flat] = multyply(new Complex(R,0) , exp(new Complex(0, n * theta)) );
                 }
             }
         }
         return new MyZernike(ORDER, pq, pqind, ZBF);
     }
 
-    public Complex multyplay(Complex a, Complex b) {        //OK - sprawdzone
+    public Complex multyply(Complex a, Complex b) {
         return new Complex(a.getReal()*b.getReal() - a.getImaginary()*b.getImaginary(),
                             a.getReal()*b.getImaginary() + a.getImaginary()*b.getReal());
     }
 
-    public Complex exp(Complex c) {     //OK - sprawdzone
+    public Complex exp(Complex c) {
         return new Complex(Math.exp(c.getReal()) * Math.cos(c.getImaginary()), Math.exp(c.getReal()) * Math.sin(c.getImaginary()));
     }
 
 
-    private Complex[] zernike_mom(int[][] I, MyZernike ZBFSTR){   //TODO sprawdzic
+    private Complex[] zernike_mom(int[][] I, MyZernike ZBFSTR){
         Complex[][][] bf = ZBFSTR.bf;
         ArrayList<int[]> pq=ZBFSTR.orders;
         int[][] id = ZBFSTR.index;
@@ -154,17 +151,17 @@ public class ZernikeMoments implements ExtractionAlgorithm {
             Complex temp = new Complex(0,0);
             for(int x=0; x<I[0].length; x++){           //TODO sprawdzic
                 for(int y=0; y<I[0].length; y++){
-                    temp = temp.add(multyplay(new Complex(I[x][y],0) , bf[x][y][flat].conjugate()));
+                    temp = temp.add(multyply(new Complex(I[x][y],0) , bf[x][y][flat].conjugate()));
                 }
             }
   //--------------------------------------------------
 
-            Z[flat] = multyplay(Z[flat],temp);
+            Z[flat] = multyply(Z[flat],temp);
         }
         return Z;
     }
 
-    public class MyZernike{     //OK - sprawdzone
+    public class MyZernike{
         int ORDER;
         ArrayList<int[]> orders;
         int[][] index;
@@ -178,7 +175,7 @@ public class ZernikeMoments implements ExtractionAlgorithm {
         }
     }
 
-    private int[] factorial(int n){     //OK - sprawdzone
+    private int[] factorial(int n){
         int[] output = new int[n+1];
         output[0] = 1;
         for(int i=1; i<=n; i++){
@@ -187,7 +184,7 @@ public class ZernikeMoments implements ExtractionAlgorithm {
         return output;
     }
 
-    private ArrayList<int[]> zernike_orderlist(int order){      //OK - sprawdzone
+    private ArrayList<int[]> zernike_orderlist(int order){
         ArrayList<int[]> PQ = new ArrayList<int[]>();
         for(int p=0; p<=order; p++){
             for(int q=0; q<=p; q++){
