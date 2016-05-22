37241cf0a34ae15fa763ce2ba218de69a1d701e3
aruziell <aruziell@gmail.com>
2016-05-22 02:36:53 +0200

    Simplify Main.main() by replacing loops with streams

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,8 @@ import tletters.knnclassification.EuclideanDistanceMeter;
 import tletters.knnclassification.KNNClassifier;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by mariuszbeltowski on 23/04/16.
@@ -25,40 +24,23 @@ public class Main {
         imageGenerator.generateImage(font, 48, letters, 0);
         GlyphExtractor glyphExtractor = new GlyphExtractor();
         glyphExtractor.setImage(imageGenerator.getGeneratedImage());
-        List<BufferedImage> lettersList = glyphExtractor.scalpel();
         ImageScaler imageScaler = new ImageScaler();
-        List<BufferedImage> scaledLettersList = new ArrayList<>();
-        for (BufferedImage image : lettersList) {
-            BufferedImage scaledImage = imageScaler.scalImage(image);
-            scaledLettersList.add(scaledImage);
-        }
         Zoning zoning = new Zoning();
-        List<Glyph> glyphList = new ArrayList<>();
-        int i = 0;
-        for (BufferedImage image : scaledLettersList) {
-            Character character = letters.charAt(i);
-            glyphList.add(new Glyph(zoning.extractFeatures(image),
-                    LanguageType.GENERAL_PL,
-                    character));
-            i++;
-        }
+        final int[] i = {0};
+        List<Glyph> glyphList = glyphExtractor.scalpel().stream()
+                .map(imageScaler::scalImage)
+                .map(image -> new Glyph(zoning.extractFeatures(image), LanguageType.GENERAL_PL, letters.charAt(i[0]++)))
+                .collect(Collectors.toList());
+
         GlyphClassifier glyphClassifier = new GlyphClassifier(new KNNClassifier<Double>(2), new EuclideanDistanceMeter<Double>(), glyphList);
         String text = "Krzysztof Misztal";
         imageGenerator.generateImage(font, 24, text, 0);
         glyphExtractor.setImage(imageGenerator.getGeneratedImage());
-        List<BufferedImage> textLettersList = glyphExtractor.scalpel();
-        List<BufferedImage> scaledTextLettersList = new ArrayList<>();
-        for (BufferedImage image : textLettersList) {
-            BufferedImage scaledImage = imageScaler.scalImage(image);
-            scaledTextLettersList.add(scaledImage);
-        }
-        List<double[]> textLettersVectorList = new ArrayList<>();
-        for (BufferedImage image : scaledTextLettersList) {
-            textLettersVectorList.add(zoning.extractFeatures(image));
-        }
-        for (double[] v : textLettersVectorList) {
-            System.out.print(glyphClassifier.classify(v));
-        }
+        glyphExtractor.scalpel().stream()
+                .map(imageScaler::scalImage)
+                .map(zoning::extractFeatures)
+                .map(glyphClassifier::classify)
+                .forEach(System.out::print);
     }
 
 }
